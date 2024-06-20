269c9e103f3cfbb65038eae21dd36bb4411cb484
Gerard-007 <gerardnwazk@gmail.com>
2024-06-20 14:24:11 +0100

    Initial commit

--- backend/controllers/weather.js
+++ backend/controllers/weather.js
@@ -22,7 +22,7 @@ const getWeather = async (req, res) => {
 
         await weatherEntry.save();
 
-        res.status(StatusCodes.OK).json(weatherData);
+        res.status(StatusCodes.OK).json(response.data);
     } catch (error) {
         console.error('Error fetching weather data:', error);
 
@@ -31,5 +31,37 @@ const getWeather = async (req, res) => {
     }
 };
 
-module.exports = { getWeather };
+
+const getWeatherPlot = async (req, res) => {
+    try {
+        const { date } = req.query;
+        const apiKey = process.env.WEATHER_API_KEY;
+        const location = 'Lagos';
+        const startDate = new Date(date);
+        const weatherDataArray = [];
+
+        for (let i = 0; i < 14; i++) {
+            const currentDate = new Date(startDate);
+            currentDate.setDate(startDate.getDate() - i);
+            const formattedDate = currentDate.toISOString().split('T')[0];
+
+            const response = await axios.get(`http://api.weatherapi.com/v1/history.json?key=${apiKey}&q=${location}&dt=${formattedDate}`);
+            const weatherData = response.data;
+            weatherDataArray.push({
+                date: formattedDate,
+                temperature: weatherData.forecast.forecastday[0].day.avgtemp_c
+            });
+        }
+
+        res.status(StatusCodes.OK).json(weatherDataArray);
+    } catch (error) {
+        console.error('Error fetching weather data:', error);
+
+        const statusCode = error.response ? error.response.status : StatusCodes.REQUEST_TIMEOUT;
+        res.status(statusCode).json({ message: 'Error fetching weather data' });
+    }
+};
+
+
+module.exports = { getWeather, getWeatherPlot };
 
--- backend/routes/weather.js
+++ backend/routes/weather.js
@@ -1,7 +1,8 @@
 const express = require('express');
 const router = express.Router()
-const {getWeather} = require("../controllers/weather")
+const {getWeather, getWeatherPlot} = require("../controllers/weather")
 
 router.route("/").get(getWeather)
+router.route('/plot').get(getWeatherPlot)
 
 module.exports = router;
