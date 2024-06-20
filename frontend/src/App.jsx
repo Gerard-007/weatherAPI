562cc82c7cdf1b9d5267c48b42a158ca6c199ece
Gerard-007 <gerardnwazk@gmail.com>
2024-06-20 22:54:47 +0100

    Initial commit

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -12,7 +12,7 @@ function App() {
         try {
             const response = await axios.get(`http://localhost:5000/api/v1/weather?date=${date}`);
             setWeather(response.data);
-            console.log(response.data)
+            console.log(response.data);(response.data);(response.data)
         } catch (error) {
             console.error('Error fetching weather data', error);
         }
@@ -29,65 +29,64 @@ function App() {
     };
 
     return (
-        <div>
-            <h1>Weather API</h1>
-            <input
-                type="date"
-                value={date}
-                onChange={(e) => setDate(e.target.value)}
-                placeholder="Enter date"
-            />
-            <button onClick={fetchWeather}>Get Weather</button>
-            <button onClick={fetchWeatherPlot}>Get Weather Plot</button>
-
-            {weather && (
-                <div>
-                  <h2>Weather Data</h2>
-                  <p>Temperature: {weather.temperature}°C</p>
-                  <p>Date: {new Date(weather.date).toLocaleDateString()}</p>
+        <div className="container mt-5 mb-5">
+          <div className="row mb-5">
+            <div className="col-md-6 text-center card">
+              <h1>Weather API</h1>
+              <div className="row">
+                <div className="col-12 my-2">
+                  <input
+                      className="form-control"
+                      type="date"
+                      value={date}
+                      onChange={(e) => setDate(e.target.value)}
+                      placeholder="Enter date"
+                  />
                 </div>
-            )}
 
+                <div className="d-grid gap-2 d-md-flex justify-content-md-end my-3">
+                  <button className="btn btn-primary" onClick={fetchWeather}>Get Weather</button>
+                  <button className="btn btn-info" onClick={fetchWeatherPlot}>Get Weather Plot</button>
+                </div>
 
+              </div>
+            </div>
+
+            <div className="col-md-6 text-center card">
+              {weather && (
+                  <div>
+                    <h2>Weather Data</h2>
+                    <p>Temperature: {weather.temperature}°C</p>
+                    <p>Date: {new Date(weather.date).toLocaleDateString()}</p>
+                  </div>
+              )}
+            </div>
+          </div>
+
+          <div className="row">
+            <div className="col-md-12 card">
             {weatherPlotData && (
-                <div>
-                    <h2>Weather Plot</h2>
-                    <Line
-                      data={{
-                        labels: weatherPlotData.map(entry => entry.date),
-                        datasets: [{
-                          label: 'Temperature (°C)',
-                          data: weatherPlotData.map(entry => entry.temperature),
-                          borderColor: 'rgba(75, 192, 192, 1)',
-                          backgroundColor: 'rgba(75, 192, 192, 0.2)',
-                          fill: true,
-                        }]
-                      }}
-                    />
-                    {/* <Line
-                        data={{
-                            labels: weatherPlotData.map(entry => entry.date),
-                            datasets: [{
-                                label: 'Temperature (°C)',
-                                data: weatherPlotData.map(entry => entry.temperature),
-                                // borderColor: 'rgba(75, 192, 192, 1)',
-                                // backgroundColor: 'rgba(75, 192, 192, 0.2)',
-                                // fill: true,
-                            }],
-                        }}
-                        options={{
-                            scales: {
-                                x: {
-                                    type: 'time',
-                                    time: {
-                                        unit: 'day'
-                                    }
-                                }
-                            }
-                        }}
-                    /> */}
-                </div>
+              <div>
+                  <h2>Weather Plot</h2>
+                  <Line
+                    data={{
+                      labels: weatherPlotData.map(entry => entry.date),
+                      datasets: [{
+                        label: 'Temperature (°C)',
+                        data: weatherPlotData.map(entry => entry.temperature),
+                        borderColor: 'rgba(75, 192, 192, 1)',
+                        backgroundColor: 'rgba(75, 192, 192, 0.2)',
+                        fill: true,
+                      }]
+                    }}
+                  />
+              </div>
             )}
+            </div>
+          </div>
+
+
+            
         </div>
     );
 }
